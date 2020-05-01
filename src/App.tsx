34603e78bde64fcb3097c246c968d3ab0f6bbc58
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-01 16:25:56 +0600

    Initializing the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Editor(currentState: any, updateState: any) {
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        <p>Editor</p>
+      </div>
+      <textarea id="editor" cols={30} rows={10}></textarea>
+    </div>
+  );
+}
+
+function MarkdownPreviewer(currentState: any) {
+  console.log(window, currentState);
+
+  let renderedMardown = (window as any).marked(currentState.currentState);
+
+  return (
+    <div className="preview-container">
+      <div className="preview-title">
+        <h1>Preview</h1>
+      </div>
+
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: renderedMardown,
+        }}
+      ></div>
+    </div>
+  );
+}
+
 function App() {
+  const [unparsedMarkdown, updateMarkdown] = useState('*Hey* **there**!');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor currentState={unparsedMarkdown} updateMarkdown={updateMarkdown} />
+      <MarkdownPreviewer currentState={unparsedMarkdown} />
     </div>
   );
 }
