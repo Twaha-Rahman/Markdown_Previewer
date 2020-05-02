0be553311f44a07903c9b72df783e99c852fb2bb
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-02 11:24:06 +0600

    Finalised the components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function Editor(state: any) {
   return (
     <div className="editor-container">
-      <div className="editor-title">
+      <div className="title">
         <p>Editor</p>
       </div>
       <textarea
@@ -12,7 +12,9 @@ function Editor(state: any) {
         onChange={(event) => {
           state.updateState(event.target.value);
         }}
-      ></textarea>
+      >
+        {state.currentState}
+      </textarea>
     </div>
   );
 }
@@ -22,7 +24,7 @@ function MarkdownPreviewer(currentState: any) {
 
   return (
     <div className="preview-container">
-      <div className="preview-title">
+      <div className="title">
         <p>Preview</p>
       </div>
 
@@ -37,11 +39,57 @@ function MarkdownPreviewer(currentState: any) {
 }
 
 function App() {
-  const [unparsedMarkdown, updateMarkdown] = useState('*Hey* **there**!');
+  const preMarkdown = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+  
+  ![React Logo w/ Text](https://goo.gl/Umyytc)`;
+
+  const [unparsedMarkdown, updateMarkdown] = useState(preMarkdown);
 
   return (
     <div className="App">
-      <h1>Welcome Live Markdown!</h1>
+      <h1>Live Markdown Previewer</h1>
       <Editor currentState={unparsedMarkdown} updateState={updateMarkdown} />
       <MarkdownPreviewer currentState={unparsedMarkdown} />
     </div>
